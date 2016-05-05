14d71cb82e52296114cdc79dfc19e2b5a8556341
Julian Kates-Harbeck <julian@Julians-MacBook-Air.local>
2016-05-04 22:47:37 -0400

    added python learning script

--- src/learn.py
+++ src/learn.py
@@ -1,5 +1,4 @@
 from pylab import *
-from __future__ import print_function
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM, SimpleRNN
