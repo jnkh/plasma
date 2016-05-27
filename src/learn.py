d483d399a99df5730e99c11a56256e3a7062db65
Julian Kates-Harbeck <julian@Julians-MacBook-Air.local>
2016-05-26 20:17:10 -0400

    outsourcing of training

--- src/learn.py
+++ src/learn.py
@@ -73,102 +73,116 @@ nn.train()
 loader = Loader(conf,nn)
 print("...done")
 
-#####################################################
-####################Training#########################
-#####################################################
-
-np.random.seed(5)
-
-##Need to import later because accessing the GPU from several processes via multiprocessing
-## gives weird errors.
-os.environ["THEANO_FLAGS"] = "device=gpu1"
-import theano
-from keras.utils.generic_utils import Progbar 
-import model_builder #from model_builder import ModelBuilder, LossHistory
-
-print('Build model...',end='')
-builder = model_builder.ModelBuilder(conf)
-train_model,test_model = builder.build_train_test_models()
-print('...done')
 
 print('Training on {} shots, testing on {} shots'.format(len(shot_list_train),len(shot_list_test)))
 
 
-#load the latest epoch we did. Returns -1 if none exist yet
-e = builder.load_model_weights(train_model)
+
+def train(conf,shot_list_train,loader):
+    #####################################################
+    ####################Training#########################
+    #####################################################
+
+    np.random.seed(5)
+
+    ##Need to import later because accessing the GPU from several processes via multiprocessing
+    ## gives weird errors.
+    os.environ["THEANO_FLAGS"] = "device=gpu1"
+    import theano
+    from keras.utils.generic_utils import Progbar 
+    import model_builder #from model_builder import ModelBuilder, LossHistory
+
+    print('Build model...',end='')
+    builder = model_builder.ModelBuilder(conf)
+    train_model,test_model = builder.build_train_test_models()
+    print('...done')
 
 
-if conf['training']['data_parallel']:
-    #elephas
-    from pyspark import SparkConf, SparkContext
-    sc = SparkContext()
-    from elephas.utils.rdd_utils import to_simple_rdd
-    from elephas.spark_model import SparkModel
-    from elephas import optimizers as elephas_optimizers
-    adam = elephas_optimizers.Adam()
-    spark_model = SparkModel(sc,train_model,optimizer=adam,frequency='batch',
-        mode='synchronous',num_workers=2)
 
+    #load the latest epoch we did. Returns -1 if none exist yet
+    e = builder.load_model_weights(train_model)
 
-
-
-num_at_once = conf['training']['num_shots_at_once']
-print('{} epochs left to go'.format(num_epochs - 1 - e))
-while e < num_epochs-1:
-    e += 1
-    print('Epoch {}/{}'.format(e+1,num_epochs))
-    pbar =  Progbar(len(shot_list_train))
-
-    #shuffle during every iteration
-    shot_list_train.shuffle() 
-    shot_sublists = shot_list_train.sublists(num_at_once)
-    for (i,shot_sublist) in enumerate(shot_sublists):
-        X_list,y_list = loader.load_as_X_y_list(shot_sublist)
-        for j,(X,y) in enumerate(zip(X_list,y_list)):
-            history = model_builder.LossHistory()
-
-            if conf['training']['data_parallel']:
-                rdd = to_simple_rdd(sc,X,y)
-                spark_model.train(rdd,
-                batch_size=Loader.get_batch_size(conf['training']['batch_size'],prediction_mode=False),
-                nb_epoch=1,verbose=1,
-                validation_split=0.0)
-
-            else:
-                #load data and fit on data
-                train_model.fit(X,y,
-                    batch_size=Loader.get_batch_size(conf['training']['batch_size'],prediction_mode=False),
-                    nb_epoch=1,shuffle=False,verbose=0,
-                    validation_split=0.0,callbacks=[history])
-                train_model.reset_states()
-
-            # print('Shots {}/{}'.format(i*num_at_once + j*1.0*len(shot_sublist)/len(X_list),len(shot_list_train)))
-            pbar.add(1.0*len(shot_sublist)/len(X_list), values=[("train loss", np.mean(history.losses))])
-            loader.verbose=False#True during the first iteration
 
     if conf['training']['data_parallel']:
-        builder.save_model_weights(spark_model,e)
-    else:
-        builder.save_model_weights(train_model,e)
+        #elephas
+        from pyspark import SparkConf, SparkContext
+        sc = SparkContext()
+        from elephas.utils.rdd_utils import to_simple_rdd
+        from elephas.spark_model import SparkModel
+        from elephas import optimizers as elephas_optimizers
+        adam = elephas_optimizers.Adam()
+        spark_model = SparkModel(sc,train_model,optimizer=adam,frequency='batch',
+            mode='synchronous',num_workers=2)
 
 
 
-    #validation
-    if conf['training']['evaluate']:
-        builder.load_model_weights(test_model)
-        for (i,shot) in enumerate(shot_list_train):
-            X,y = loader.load_as_X_y(shot,prediction_mode=True)
-            print(test_model.evaluate(X,y,batch_size=Loader.get_batch_size(conf['training']['batch_size'],prediction_mode=True)))
 
-print('...done')
+    num_at_once = conf['training']['num_shots_at_once']
+    print('{} epochs left to go'.format(num_epochs - 1 - e))
+    while e < num_epochs-1:
+        e += 1
+        print('Epoch {}/{}'.format(e+1,num_epochs))
+        pbar =  Progbar(len(shot_list_train))
+
+        #shuffle during every iteration
+        shot_list_train.shuffle() 
+        shot_sublists = shot_list_train.sublists(num_at_once)
+        for (i,shot_sublist) in enumerate(shot_sublists):
+            X_list,y_list = loader.load_as_X_y_list(shot_sublist)
+            for j,(X,y) in enumerate(zip(X_list,y_list)):
+                history = model_builder.LossHistory()
+
+                if conf['training']['data_parallel']:
+                    rdd = to_simple_rdd(sc,X,y)
+                    spark_model.train(rdd,
+                    batch_size=Loader.get_batch_size(conf['training']['batch_size'],prediction_mode=False),
+                    nb_epoch=1,verbose=1,
+                    validation_split=0.0)
+
+                else:
+                    #load data and fit on data
+                    train_model.fit(X,y,
+                        batch_size=Loader.get_batch_size(conf['training']['batch_size'],prediction_mode=False),
+                        nb_epoch=1,shuffle=False,verbose=0,
+                        validation_split=0.0,callbacks=[history])
+                    train_model.reset_states()
+
+                # print('Shots {}/{}'.format(i*num_at_once + j*1.0*len(shot_sublist)/len(X_list),len(shot_list_train)))
+                pbar.add(1.0*len(shot_sublist)/len(X_list), values=[("train loss", np.mean(history.losses))])
+                loader.verbose=False#True during the first iteration
+
+        if conf['training']['data_parallel']:
+            builder.save_model_weights(spark_model,e)
+        else:
+            builder.save_model_weights(train_model,e)
 
 
 
-reload(theano)
-import theano.sandbox.cuda
-theano.sandbox.cuda.use('cpu')
-reload(model_builder)
+        #validation
+        if conf['training']['evaluate']:
+            builder.load_model_weights(test_model)
+            for (i,shot) in enumerate(shot_list_train):
+                X,y = loader.load_as_X_y(shot,prediction_mode=True)
+                print(test_model.evaluate(X,y,batch_size=Loader.get_batch_size(conf['training']['batch_size'],prediction_mode=True)))
+
+    print('...done')
+
+
+
+
+import multiprocessing as old_mp
 import pathos.multiprocessing as mp
+p = old_mp.Process(target = train,args=(conf,shot_list_train,loader))
+p.start()
+p.join()
+
+
+
+
+os.environ["THEANO_FLAGS"] = "device=cpu"
+import theano
+from keras.utils.generic_utils import Progbar 
+import model_builder #from model_builder import ModelBuilder, LossHistory
 builder = model_builder.ModelBuilder(conf)
 
 #####################################################
@@ -191,7 +205,7 @@ disruptive_test= []
 
 
 
-def make_predictions(conf,model,shot_list,num_total):
+def make_predictions(conf,shot_list,num_total):
     y_prime = []
     y_gold = []
     disruptive = []
@@ -208,6 +222,7 @@ def make_predictions(conf,model,shot_list,num_total):
         y_gold.append(y)
         disruptive.append(is_disruptive)
     print('Finished Predictions in {} seconds'.format(time.time()-start_time))
+    return y_prime,y_gold,disruptive
 
 
 
@@ -231,8 +246,8 @@ def make_single_prediction(shot,builder,loader):
     return y_prime,y_gold,disruptive
 
 
-y_prime_train,y_gold_train,disruptive_train = make_predictions(conf,test_model,shot_list_train,num_shots)
-y_prime_train,y_gold_train,disruptive_train = make_predictions(conf,test_model,shot_list_test,num_shots)
+y_prime_train,y_gold_train,disruptive_train = make_predictions(conf,shot_list_train,num_shots)
+y_prime_test,y_gold_test,disruptive_test = make_predictions(conf,shot_list_test,num_shots)
 
 
 
