22b7663bef3f082cdb4dbe002c162ea9978ec491
Julian Kates-Harbeck <julian@Julians-MacBook-Air.local>
2016-06-03 18:35:15 -0400

    added ttd mode

--- src/performance_analysis_utils.py
+++ src/performance_analysis_utils.py
@@ -464,13 +464,13 @@ class PerformanceAnalyzer():
                 ax.plot((truth)[::-1],label='ground truth')
             else:
                 ax.semilogy((truth+0.001)[::-1],label='ground truth')
+                ax.set_ylim([1e-5,1.1e0])
             ax.plot(prediction[::-1],'g',label='neural net prediction')
             ax.axvline(self.T_min_warn,color='r',label='max warning time')
             ax.axvline(self.T_max_warn,color='r',label='min warning time')
             ax.axhline(P_thresh_opt,color='k',label='trigger threshold')
             # ax.set_xlabel('TTD [ms]')
             # ax.legend(loc = 'best',fontsize=10)
-            ax.set_ylim([1e-5,1.1e0])
             setp(ax.get_yticklabels(),fontsize=7)
             # ax.grid()           
             if save_fig:
