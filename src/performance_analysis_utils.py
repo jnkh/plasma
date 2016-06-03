d37474c80c67bbed8d762269ec8d49ceeaf77bb5
Julian Kates-Harbeck <julian@Julians-MacBook-Air.local>
2016-06-02 23:43:38 -0400

    better plotting

--- src/performance_analysis_utils.py
+++ src/performance_analysis_utils.py
@@ -443,7 +443,7 @@ class PerformanceAnalyzer():
             else:
                 print('non disruptive')
 
-            f,axarr = subplots(len(signals.T)+1,1,sharex=True,figsize=(15,15))
+            f,axarr = subplots(len(signals.T)+1,1,sharex=True,figsize=(13,13))
             for (i,sig) in enumerate(signals.T):
                 ax = axarr[i]
                 ax.plot(sig[::-1],label = labels[i])
