9d1329e40d46bdecb841fada768bb4aa340018bf
Julian Kates-Harbeck <julian@Julians-MacBook-Air.local>
2016-06-04 04:27:36 -0400

    plotting

--- src/performance_analysis_utils.py
+++ src/performance_analysis_utils.py
@@ -471,10 +471,11 @@ class PerformanceAnalyzer():
             ax = axarr[-1] 
             if self.pred_ttd:
                 ax.semilogy((-truth+0.0001)[::-1],label='ground truth')
+                ax.plot(-prediction[::-1]+0.0001,'g',label='neural net prediction')
             else:
                 ax.semilogy((truth+0.001)[::-1],label='ground truth')
+                ax.plot(prediction[::-1],'g',label='neural net prediction')
                 ax.set_ylim([1e-5,1.1e0])
-            ax.plot(-prediction[::-1]+0.0001,'g',label='neural net prediction')
             ax.axvline(self.T_min_warn,color='r',label='max warning time')
             ax.axvline(self.T_max_warn,color='r',label='min warning time')
             ax.axhline(P_thresh_opt,color='k',label='trigger threshold')
