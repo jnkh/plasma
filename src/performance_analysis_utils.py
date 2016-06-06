a93b9fa142d056d2055759aaf114b19bb9488586
Julian Kates-Harbeck <julian@Julians-MacBook-Air.local>
2016-06-05 21:11:32 -0400

    plotting

--- src/performance_analysis_utils.py
+++ src/performance_analysis_utils.py
@@ -497,7 +497,7 @@ class PerformanceAnalyzer():
         figure()
         # semilogx(P_thresh_range,accuracy_range,label="accuracy")
         if self.pred_ttd:
-            semilogx(P_thresh_range,missed_range,'r',label="missed")
+            semilogx(abs(P_thresh_range[::-1]),missed_range,'r',label="missed")
         else:
             semilogx(P_thresh_range,missed_range,'r',label="missed")
         plot(P_thresh_range,fp_range,'k',label="false positives")
