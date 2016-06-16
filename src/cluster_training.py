4fec81b9a07f974cdd7095e391be728ce283bbf7
Julian Kates-Harbeck <juliankh91@gmail.com>
2016-06-15 22:49:31 -0400

    cluster training

--- src/cluster_training.py
+++ src/cluster_training.py
@@ -5,13 +5,14 @@ import numpy as np
 import random
 import time
 
-import tensorflow as tf 
+#import tensorflow as tf 
 from keras.models import Sequential
 from keras.layers.wrappers import TimeDistributed
 from keras.layers.core import Dense, Activation, Dropout
 from keras.layers.recurrent import LSTM
 from keras.utils.generic_utils import Progbar 
 from keras.callbacks import Callback
+from keras.optimizers import Adam
  
 from data_processing import *
 from pylab import *
@@ -22,11 +23,14 @@ def build_model(predict,batch_size,length,featurelen):
         batch_size = length = 1
     model = Sequential()
     model.add(LSTM(10 ,return_sequences=True, batch_input_shape=(batch_size, length , featurelen), stateful=True))
-    model.add(Dropout(0.1))
-    model.add(LSTM(10 , return_sequences=True,stateful=True))
-    model.add(Dropout(0.1))
+    # model.add(Dropout(0.1))
+    # model.add(LSTM(10 , return_sequences=True,stateful=True))
+    # model.add(Dropout(0.1))
     model.add(TimeDistributed(Dense( featurelen )))
-    model.compile(loss='mse', optimizer='rmsprop')
+
+
+    opt = Adam(lr = 0.00001)
+    model.compile(loss='mse', optimizer='adam')
     model.reset_states()
     return model
 
@@ -65,25 +69,25 @@ class LossHistory(Callback):
         self.losses.append(logs.get('loss'))
 
 def get_mock_data():
-    x_raw = cumsum(randn(10000))
+    x_raw = cumsum(randn(1000))
     x_raw = vstack([x_raw]*1)
     x_raw = x_raw.T
-    x = x[10:]
-    y = x[:10]
+    x_raw = 1.0*(x_raw - x_raw.min()) / (x_raw.max() - x_raw.min())
+    x = x_raw[10:]
+    y = x_raw[:-10]
     return x,y
 
-xx,yy = get_mock_data()
+def rms(y1,y2):
+    return mean((y1-y2)**2)
 
-
-num_shots = 10
-shots = [(xx,yy) for _ in range(num_shots)]
-
+plotting = False
+num_shots = 50
 length = 20
 batch_size = length
 skip = 1
 return_sequences = True
 
-featurelen = yy.shape[1]
+featurelen = get_mock_data()[1].shape[1]
 
 
 
@@ -94,8 +98,9 @@ testing_model = build_model(True,batch_size,length,featurelen)
 
  
 print('starting training')
-num_epochs = 3
+num_epochs = 10
 for e in range(num_epochs):
+    shots = [get_mock_data() for _ in range(num_shots)]
     print('epoch - ',e+1)
     startTime = time.time()
     pbar =  Progbar(len(shots))
@@ -112,8 +117,9 @@ for e in range(num_epochs):
     
 
     model.save_weights('./tmp1/cluster_training.%d.h5' % e,overwrite=True)
-    testing_model.load_weights('./tmp1/keras_char_rnn.%d.h5' % e)
+    testing_model.load_weights('./tmp1/cluster_training.%d.h5' % e)
     y_pred = []
+    xx,yy = get_mock_data()
     for i in range(len(xx)):
         C = np.zeros([1,1,featurelen])
         C[0,0,:] = xx[i,:]
@@ -121,14 +127,17 @@ for e in range(num_epochs):
         pred = pred[0,0,:] # We have to pass in an entire batch at a time, but we only care about the first since we are only generation one letter
         y_pred.append(pred)
     y_pred = array(y_pred)
-    figure(e)
-    plot(xx,'-b')
-    plot(yy,'-g')
-    plot(y_pred,'-r')
-    show()
-    testing_model.reset_states()
-
     totalTime = time.time() - startTime
     print('Completed epoch in ',totalTime,' seconds')
+    print('===============================')
+    print ('Test Loss: {}'.format(rms(y_pred,yy)))
+    if plotting:
+        figure(e)
+        plot(xx,'-b')
+        plot(yy,'-g')
+        plot(y_pred,'-r')
+        show()
+    testing_model.reset_states()
+
     print()
 print('training complete')
