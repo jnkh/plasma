d609e3d3f68718dcaf86d672fab5899a1f274b19
Julian Kates-Harbeck <juliankh91@gmail.com>
2016-07-11 00:06:12 -0400

    mpi training

--- src/data_processing.py
+++ src/data_processing.py
@@ -663,7 +663,6 @@ class Loader(object):
             shot_sublists = shot_list.sublists(num_at_once,equal_size=True)
             num_total = len(shot_sublists)
             for (i,shot_sublist) in enumerate(shot_sublists):
-                print('Shot sublist: {}'.format(str(shot_sublist)))
                 num_so_far = i
                 X_list,y_list = loader.load_as_X_y_list(shot_sublist)
                 for j,(X,y) in enumerate(zip(X_list,y_list)):
