e2bc4d46f66aeee68affbaa97d2322ceab89e977
Julian Kates-Harbeck <juliankh91@gmail.com>
2016-07-12 16:54:29 -0400

    temperature profiles

--- src/get_mdsplus_data.py
+++ src/get_mdsplus_data.py
@@ -46,6 +46,13 @@ elif machine == 'jet':
 	signal_paths += ['jpf/df/g1r-lid:{:03d}'.format(i) for i in range(1,9)]
 
 
+
+	#radiation signals
+	#vertical signals, don't use signal 16 and 23
+	signal_paths += ['jpf/db/b5vr-pbol<raw:{:03d}'.format(i) for i in range(1,28) if (i != 16 and i != 23)]
+	signal_paths += ['jpf/db/b5hr-pbol<raw:{:03d}'.format(i) for i in range(1,24)]
+
+
 	#ece temperature profiles
 	# signal_paths += ['jpf/de/kk3/p{:03d}'.format(285 + 5*i) for i in range(1,14)]
 
@@ -53,10 +60,6 @@ elif machine == 'jet':
 	signal_paths += ['ppf/kk3/ra{:02d}'.format(i) for i in range(1,97)]
 	signal_paths += ['ppf/kk3/rc{:02d}'.format(i) for i in range(1,97)]
 
-	#radiation signals
-	#vertical signals, don't use signal 16 and 23
-	signal_paths += ['jpf/db/b5vr-pbol<raw:{:03d}'.format(i) for i in range(1,28) if (i != 16 and i != 23)]
-	signal_paths += ['jpf/db/b5hr-pbol<raw:{:03d}'.format(i) for i in range(1,24)]
 
 
 
