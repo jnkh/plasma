1cd71ae6d364a5d360e909f94e250c153e2e36f8
Kyle Gerard Felker <kfelker@princeton.edu>
2016-08-22 18:38:30 -0400

    Expanded install_dependencies.sh to include mpi4py, commented out MDSPlus. Both are optional packages depending on desired activity

--- src/install_dependencies.sh
+++ src/install_dependencies.sh
@@ -1,4 +1,5 @@
 pip install git+https://github.com/uqfoundation/pathos.git@master --user
 pip install keras --user
 pip install elephas --user
-pip install mdsplus --user
+#pip install mdsplus --user #only required for downloading data, not needed if data already exists locally
+pip install mpi4py --user #needed for parallel GPU training
